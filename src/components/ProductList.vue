302e94ccad15f68e09cd3793537e33bac5c332e6
alexey <khrischatyy@gmail.com>
2021-04-13 18:53:05 +0300

    3 module/hometask

---
+++ src/components/ProductList.vue
@@ -0,0 +1,16 @@
+<template>
+    <product-item :products="products"></product-item>
+</template>
+
+<script>
+import ProductItem from './ProductItem';
+export default {
+  name: 'ProductList',
+  components: { ProductItem },
+  props:['products'],
+};
+</script>
+
+<style scoped>
+
+</style>
