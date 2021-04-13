5d9b76bc126847b201d81b7e9be67d713e00dbe3
alexey <khrischatyy@gmail.com>
2021-04-13 18:53:05 +0300

    3 module/hometask

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+import { message, data } from './first';
+import sayWord from './second';
+
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
+
+alert(message);
+alert(data.message);
+sayWord('Hello');
