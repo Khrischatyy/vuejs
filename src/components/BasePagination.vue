ab62ddabbb59ba1e537688ec2b7dd0207e166515
alexey <khrischatyy@gmail.com>
2021-04-19 11:37:24 +0300

    3 module/hometask

---
+++ src/components/BasePagination.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="catalog__pagination pagination">
+    <li class="pagination__item">
+      <a class="pagination__link pagination__link--arrow pagination__link--disabled" aria-label="Предыдущая страница">
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-left"></use>
+        </svg>
+      </a>
+    </li>
+    <li v-for="pageNumber in pages"  class="pagination__item">
+      <a href="#" @click.prevent="paginate(pageNumber)" class="pagination__link" :class="{'pagination__link--current':pageNumber === page}">
+        {{pageNumber}}
+      </a>
+    </li>
+    <li class="pagination__item">
+      <a class="pagination__link pagination__link--arrow" href="#" aria-label="Следующая страница">
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'page',
+    event: 'paginate',
+  },
+  name: 'BasePagination',
+  props:['page','count','perPage'],
+  computed: {
+    pages() {
+      return Math.ceil(this.count / this.perPage);
+    }
+  },
+  methods: {
+    paginate(pageNumber) {
+      this.$emit('paginate', pageNumber);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
