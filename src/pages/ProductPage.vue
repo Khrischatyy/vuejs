2934cf765827ff1503eb598f82d8fc95efb24672
Алексей Хрищатый <tony@machine-2.local>
2021-06-11 16:27:34 +0300

    second lesson of 6 mudule

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -3,14 +3,14 @@
     <div class="content__top">
       <ul class="breadcrumbs">
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" @click.prevent="gotoPage('main')">
+          <router-link class="breadcrumbs__link" :to="{name: 'main'}">
             Каталог
-          </a>
+          </router-link>
         </li>
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" href="#" @click.prevent="gotoPage('main')">
+          <router-link class="breadcrumbs__link" :to="{name: 'main'}">
             {{ category.title }}
-          </a>
+          </router-link>
         </li>
         <li class="breadcrumbs__item">
           <a class="breadcrumbs__link">
@@ -209,13 +209,12 @@ import numberFormat from '@/helpers/numberFormat';
 
 export default {
   name: 'ProductPage',
-  props: ['pageParams'],
   filters: {
     numberFormat,
   },
   computed: {
     product() {
-      return data.find((product) => product.id === this.pageParams.id);
+      return data.find((product) => product.id === +this.$route.params.id);
     },
     category() {
       return categories.find((category) => category.id === this.product.categoryID);
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,37 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import MainPage from '@/pages/MainPage.vue';
+import NotFoundPage from '@/pages/NotFoundPage.vue';
+import ProductPage from '@/pages/ProductPage.vue';
+import Cart from '@/pages/Cart.vue';
 
 Vue.use(VueRouter);
 
-const router = new VueRouter();
+const routes = [
+  {
+    name: 'main',
+    component: MainPage,
+    path: '/',
+  },
+  {
+    name: 'product',
+    component: ProductPage,
+    path: '/product/:id',
+  },
+  {
+    name: 'cart',
+    component: Cart,
+    path: '/cart',
+  },
+  {
+    name: 'NotFound',
+    component: NotFoundPage,
+    path: '*',
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
 
 export default router;
