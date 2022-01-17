f22b25f0d8b0ae12919d61543fec99c3bdeccd49
alex <marvel14bigben14h@gmail.com>
2022-01-17 02:58:29 -0500

    matvey

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import MainPage from '@/pages/MainPage.vue';
 import NotFoundPage from '@/pages/NotFoundPage.vue';
 import ProductPage from '@/pages/ProductPage.vue';
 import Cart from '@/pages/Cart.vue';
-
+ 
 Vue.use(VueRouter);
 
 const routes = [
